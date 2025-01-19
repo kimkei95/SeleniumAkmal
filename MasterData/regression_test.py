251e8375b1aae6344a364dd06a0c210260c92d7c
kimkei95 <72209026+kimkei95@users.noreply.github.com>
2025-01-19 23:26:40 +0700

    adjust regression script

--- MasterData/regression_test.py
+++ MasterData/regression_test.py
@@ -2,8 +2,10 @@ import unittest
 from HtmlTestRunner import HTMLTestRunner
 from MasterData.Bank.TambahAkunBank import tambah_bank
 from MasterData.Bank.EditAkunBank import edit_abank
+from MasterData.Kelas.TambahKelas import tambah_kelas
+from MasterData.Kelas.EditKelas import edit_kelas
 import logging
-
+import os
 # Menyiapkan logging
 logging.basicConfig(level=logging.INFO)  # Set log level ke INFO agar informasi dicetak di console
 
@@ -36,17 +38,49 @@ class TestEditBank(unittest.TestCase):
                 continue  # Lanjut ke tes berikutnya
 
 
+class TestTambahKelas(unittest.TestCase):
+    def test_tambah_kelas(self):
+        for i in range(3):
+            logging.info(
+                f"Menjalankan Tes Tambah Kelas ke-{i + 1}..."
+            )
+            try:
+              tambah_kelas()
+              logging.info(f"Tes Tambah kelas ke-{i + 1} berhasil")
+            except Exception as e:
+                logging.error(f"Tes Tambah Kelas ke-{i + 1} gagal: {e}")
+                continue
+class TestEditKelas(unittest.TestCase):
+    def test_edit_kelas(self):
+        for i in range(3):
+            logging.info(
+                f"Menjalankan Tes Edit Kelas ke-{i + 1}..."
+            )
+            try:
+              edit_kelas()
+              logging.info(f"Tes Edit Kelas ke-{i + 1} berhasil")
+            except Exception as e:
+                logging.error(f"Tes Edit Kelas ke-{i + 1} gagal: {e}")
+                continue
+
+
+
 def run_regression_tests():
     # Memuat semua tes untuk tambah bank dan edit bank
-    test_suite = unittest.TestLoader().loadTestsFromTestCase(
-        TestTambahBank) + unittest.TestLoader().loadTestsFromTestCase(TestEditBank)
+    suite_tambah_bank = unittest.TestLoader().loadTestsFromTestCase(TestTambahBank)
+    suite_edit_bank = unittest.TestLoader().loadTestsFromTestCase(TestEditBank)
+    test_suite = unittest.TestSuite([suite_tambah_bank, suite_edit_bank])
 
     report_path = r"C:\Users\akmal\Downloads\Selenium\regression_test_report.html"
+    report_dir = os.path.dirname(report_path)  # Ambil path direktori
+    if not os.path.exists(report_dir):
+        os.makedirs(report_dir)  # Buat direktori jika belum ada
+
     logging.info(f"Menulis laporan di: {report_path}")  # Log untuk mengecek path file
     try:
         with open(report_path, "w") as report_file:
             runner = HTMLTestRunner(stream=report_file, verbosity=2)
-            runner.run(test_suite)
+            runner.run(test_suite)  # Jalankan suite yang digabungkan
         logging.info("Laporan berhasil dibuat")
     except Exception as e:
         logging.error(f"Gagal membuat laporan: {e}")
